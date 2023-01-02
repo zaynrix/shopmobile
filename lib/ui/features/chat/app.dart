2122d3e89822e85b4f48a3962fd557a7b9ff4cac
mac <zayns1914@gmail.com>
2023-01-02 16:06:14 +0200

    refactor

--- lib/ui/features/chat/app.dart
+++ lib/ui/features/chat/app.dart
@@ -8,6 +8,7 @@ const String apiKey= "sc9yck3acu5u";
 var logger = log.Logger();
 
 
+
 extension StreamChatContext on BuildContext{
   String? get currentUserImage => currentUser?.image;
   User? get currentUser => StreamChatCore.of(this).currentUser;
