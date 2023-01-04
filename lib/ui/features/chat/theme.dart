97dbb5ff49258f54e9bf537d6fcbfa0e4b343bed
mac <zayns1914@gmail.com>
2023-01-04 16:37:20 +0200

    refactor

--- lib/ui/features/chat/theme.dart
+++ lib/ui/features/chat/theme.dart
@@ -21,6 +21,8 @@ abstract class _LightColors {
   static const card = AppColors.cardLight;
 }
 
+
+
 abstract class _DarkColors {
   static const background = Color(0xFF1B1E1F);
   static const card = AppColors.cardDark;
